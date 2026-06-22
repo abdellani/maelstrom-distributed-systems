5f50e450225e4d68cf6a5bf3fe68a4f62fff6644
Abdellani <abdellani@gmail.com>
2026-06-22 08:58:07 +0100

    impelement broadcast

--- src/main.go
+++ src/main.go
@@ -14,15 +14,31 @@ func main() {
 	n := maelstrom.NewNode()
 	nodeId := rand.IntN(100)
 	seqNum := 0
-	var mtx sync.Mutex
+	receivedNumbers := []int{}
+	var seqMtx sync.Mutex
+	var BCMtx sync.RWMutex
 
 	generateID := func() string {
-		mtx.Lock()
-		defer mtx.Unlock()
+		seqMtx.Lock()
+		defer seqMtx.Unlock()
 		seqNum++
 		id := fmt.Sprintf("%d_%d", nodeId, seqNum)
 		log.Printf("id: %s\n", id)
 		return id
+	}
+
+	addNumToReceivedBC := func(msg int) {
+		BCMtx.Lock()
+		defer BCMtx.Unlock()
+		receivedNumbers = append(receivedNumbers, msg)
+	}
+
+	getReceivedNumbers := func() *[]int {
+		BCMtx.RLock()
+		defer BCMtx.RUnlock()
+		nums := make([]int, len(receivedNumbers))
+		copy(nums, receivedNumbers)
+		return &nums
 	}
 
 	n.Handle("echo", func(msg maelstrom.Message) error {
@@ -43,6 +59,45 @@ func main() {
 		body["type"] = "generate_ok"
 		body["id"] = generateID()
 		return n.Reply(msg, body)
+	})
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body struct {
+			Type    string `json:"type"`
+			Message int    `json:"message"`
+		}
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		addNumToReceivedBC(body.Message)
+		respBody := map[string]string{
+			"type": "topology_ok",
+		}
+
+		respBody["type"] = "broadcast_ok"
+		return n.Reply(msg, respBody)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		nums := getReceivedNumbers()
+		body["type"] = "read_ok"
+		body["messages"] = *nums
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		respBody := map[string]string{
+			"type": "topology_ok",
+		}
+		return n.Reply(msg, respBody)
 	})
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
